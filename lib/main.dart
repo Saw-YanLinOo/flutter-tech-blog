ad217264d734865d0e0cd5d641a069e6c9f698f1
Saw-YanLinOo <yly80103@gmail.com>
2023-05-05 20:52:22 +0630

    ✨ feat: init home bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:techblog/presentation/screens/home/home_screen.dart';
 import 'package:techblog/presentation/screens/profile/profile_screen.dart';
 
@@ -21,20 +22,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Teach Blog',
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData.light(
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [],
+      child: MaterialApp(
+        title: 'Teach Blog',
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.system,
+        theme: ThemeData.light(
+          useMaterial3: true,
+        ),
+        darkTheme: ThemeData.dark(
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
       ),
-      darkTheme: ThemeData.dark(
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
     );
   }
 }
