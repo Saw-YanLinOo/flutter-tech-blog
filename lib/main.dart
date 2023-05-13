5b2a5d6e5f94a50f4d0df0fa2c4ea90062310689
Saw-YanLinOo <yly80103@gmail.com>
2023-05-13 12:55:49 +0630

    feat: add offline data for blog list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:techblog/core/local/my_share_perference.dart';
 import 'package:techblog/presentation/bloc/home/blog_bloc.dart';
 import 'package:techblog/presentation/bloc/theme/theme_bloc.dart';
@@ -7,11 +9,18 @@ import 'package:techblog/presentation/bloc/theme/theme_state.dart';
 import 'package:techblog/presentation/screens/home/home_screen.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:path_provider/path_provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   setPathUrlStrategy();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
   await MySharePerference().init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
--- lib/presentation/bloc/home/blog_bloc.dart
+++ lib/presentation/bloc/home/blog_bloc.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:techblog/domain/models/blog.dart';
 import 'package:techblog/presentation/bloc/home/blog_state.dart';
 import 'package:techblog/presentation/bloc/home/blog_event.dart';
-
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import '../../../domain/repository/tech_blog_repository_impl.dart';
 
-class BlogBloc extends Bloc<BlogEvent, BlogState> {
+class BlogBloc extends HydratedBloc<BlogEvent, BlogState> {
   BlogBloc() : super(GetAllBlogInitialState()) {
     on<GetAllBlogEvent>(_getAllBlogEvent);
   }
@@ -18,6 +19,27 @@ class BlogBloc extends Bloc<BlogEvent, BlogState> {
       emit(GetAllBlogSuccessState(blogList: response));
     } catch (e) {
       emit(GetAllBlogFailStae(e: e));
+    }
+  }
+
+  @override
+  BlogState? fromJson(Map<String, dynamic> json) {
+    try {
+      final blogList =
+          (json['blogs'] as List).map((json) => Blog.fromJson(json)).toList();
+
+      return GetAllBlogSuccessState(blogList: blogList);
+    } catch (_) {
+      return null;
+    }
+  }
+
+  @override
+  Map<String, dynamic>? toJson(BlogState state) {
+    if (state is GetAllBlogSuccessState) {
+      return {'blogs': state.blogList.map((user) => user.toJson()).toList()};
+    } else {
+      return null;
     }
   }
 }
